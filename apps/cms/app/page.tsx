0077334ad2c6e2c45e761b575dad934497dea8c6
ljq0226 <1035821043@qq.com>
2022-11-09 11:07:50 +0800

    init

---
+++ apps/cms/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Button } from 'ui'
+export default function Home() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  )
+}
